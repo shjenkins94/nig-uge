54b220dc09c91cdb6d73391e14d2d071107ae48f
shjenkins94 <shjenkins94@gmail.com>
2020-04-01 19:03:49 +0900

    Removed cluster from qsub submit. Added functions

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # nig-uge
-Cookiecutter for creating snakemake profiles for the NIG Supercomputer
+
+Snakemake cookiecutter profile for running jobs on the NIG Supercomputer
+Derived from  
+[jaicher/snakemake-qsub][qsub]  
+ and  
+[broadinstitute/snakemake-broad-uger][broad].
+
+Deploy using [cookiecutter][cookiecutter-repo] (easily installed using conda or
+pip) by running:
+
+   [qsub]: https://github.com/jaicher/snakemake-sync-bq-sub
+   [broad]: https://github.com/broadinstitute/snakemake-broad-uger
+   [cookiecutter-repo]: https://github.com/audreyr/cookiecutter
+
+```
+# make sure configuration directory snakemake looks for profiles in exists
+mkdir -p ~/.config/snakemake
+# use cookiecutter to create a profile in the config directory
+cookiecutter --output-dir ~/.config/snakemake gh:jaicher/snakemake-qsub
+```
+
+This command will prompt for parameters to set.  It will ask to change default
+snakemake parameters, log directories. It will ask for a default queue for job
+submissions (if left empty, by default it will not add a flag for the queue).
+It will finally ask what the desired profile name is.
+
+Once complete, this will allow you to run Snakemake with the cluster
+configuration using the `--profile` flag. For example, if the profile name
+was `cluster-qsub`, then you can run:
+
+```
+snakemake --profile cluster-qsub {...}
+```
+
+## Specification of resources/cluster settings
+
+Individual snakemake rules can have the following parameters specified in the
+Snakemake file:
++ `threads`: the number of threads needed for the job. If not specified,
+  assumed to be 1.
++ `resources`
+    - `mem_mb`: the memory required for the rule in megabytes, which will be
+      requested if present
+
--- {{cookiecutter.profile_name}}/qsub-submit.py
+++ {{cookiecutter.profile_name}}/qsub-submit.py
@@ -9,123 +9,100 @@ cluster parameters:
 + `resources`
     - `mem_mb`: Expected memory requirements in megabytes. Overrides
       cluster.mem_mb
-+ `cluster`
-    - `mem_mb`: Memory that will be requested for the cluster for the job.
-      Overriden by resources.mem_mb, if present.
-      `resources`
-    - `queue`: Which queue to run job on
-    - `logdir`: Where to log stdout/stderr from cluster command
-    - `output`: Name of stdout logfile
-    - `error`: Name of stderr logfile
-    - `jobname`: Job name (with wildcards)
-
-Author: Joseph K Aicher
 """
 
+import os
+import re
 import sys  # for command-line arguments (get jobscript)
 import subprocess
 from pathlib import Path  # for path manipulation
 from snakemake.utils import read_job_properties  # get info from jobscript
 
+
+# Create a job name. Defaults to group name, then rule plus wildcards
+def get_job_name(job: dict) -> str:
+    if job.get("type", "") == "group":
+        groupid = job.get("groupid", "group")
+        jobid = job.get("jobid", "").split("-")[0]
+        jobname = "{groupid}_{jobid}".format(groupid=groupid, jobid=jobid)
+    else:
+        wildcards = job.get("wildcards", {})
+        wildcards_str = ("_".join(wildcards.values()) or "unique")
+        name = job.get("rule", "") or "rule"
+        jobname = "smk.{0}.{1}".format(name, wildcards_str)
+    return jobname
+
+
+# get the resources part of the command
+def generate_resources_command(job: dict) -> str:
+    # get values
+    threads = job.get("threads", 1)
+    resources = job.get("resources", {})
+    # start by requesting threads in smp if threads > 1
+    if threads > 1:
+        thread_cmd = "-pe smp {threads}"
+    else:
+        thread_cmd = ""
+    mem_mb = resources.get("mem_mb", int({{cookiecutter.default_mem_mb}}))
+    mem_cmd = "-l s_vmem={mem_mb}M -l mem_req={mem_mb}M".format(mem_mb=mem_mb)
+    if (threads >= int({{cookiecutter.reserve_min_threads}}) or
+            mem_mb >= int({{cookiecutter.reserve_min_mem_mb}})):
+        reserve_cmd = "-R yes"
+    else:
+        reserve_cmd = ""
+    res_cmd = "{thread_cmd} {mem_cmd} {reserve_cmd}".format(
+        thread_cmd=thread_cmd,
+        mem_cmd=mem_cmd,
+        reserve_cmd=reserve_cmd)
+    return (res_cmd)
+
+
+def get_log_files(jobname: str) -> str:
+    # get the name of the job
+    jobname = get_job_name(job)
+    # determine names to pass through for job name, logfiles
+    log_dir = "{{cookiecutter.default_cluster_logdir}}"
+    # get the output file name
+    out_log = "{}.out".format(jobname)
+    err_log = "{}.err".format(jobname)
+    # get logfile paths
+    out_log_path = str(Path(log_dir).joinpath(out_log))
+    err_log_path = str(Path(log_dir).joinpath(err_log))
+    log_cmd = "-o {out} -e {err} -N {name}".format(
+        out=out_log_path,
+        err=err_log_path,
+        name=jobname
+    )
+    return(log_cmd)
+
+
 # get the jobscript (last argument)
 jobscript = sys.argv[-1]
+
 # read the jobscript and get job properties
 job = read_job_properties(jobscript)
-
-# get the cluster properties
-cluster = job.get("cluster", dict())
-
-# get job information
-# get the rule
-rule = job.get("rule", "jobname")
-# get the wildcards
-wildcards = job.get("wildcards", dict())
-wildcards_str = ";".join("{}={}".format(k, v) for k, v in wildcards.items())
-if not wildcards_str:
-    # if there aren't wildcards, this is a unique rule
-    wildcards_str = "unique"
-
-# get resources information
-# get the number of threads the job wants
-threads = job.get("threads", 1)
-# get the resource properties
-resources = job.get("resources", dict())
-# get the memory usage in megabytes that we will request
-mem_mb = resources.get(
-    "mem_mb",  # first see if the job itself specifies the memory it needs
-    # if not, check if the cluster configuration gives guidance
-    cluster.get(
-        "mem_mb",  # request what the cluster says, if it says anything
-        int({{cookiecutter.default_mem_mb}})  # otherwise, use default value
-    )
-)
-mem_per_thread = round(mem_mb / threads, 2)  # per thread...
-# get the expected runtime in minutes
-runtime = cluster.get(
-    "runtime",
-    None
-)
-
-# determine names to pass through for job name, logfiles
-log_dir = cluster.get("logdir", "{{cookiecutter.default_cluster_logdir}}")
-# get the name of the job
-jobname = cluster.get("jobname", "smk.{0}.{1}".format(rule, wildcards_str))
-# get the output file name
-out_log = cluster.get("output", "{}.out".format(jobname))
-err_log = cluster.get("error", "{}.err".format(jobname))
-# get logfile paths
-out_log_path = str(Path(log_dir).joinpath(out_log))
-err_log_path = str(Path(log_dir).joinpath(err_log))
-
-# get the queue to run the job on
-queue = cluster.get("queue", "{{cookiecutter.default_queue}}")
-
-# set name/log information
-jobinfo_cmd = (
-    "-o {out_log_path:q} -e {err_log_path:q}"
-    " -N {jobname:q}"
-)
-
-# set up resources part of command
-# start by requesting threads in smp if threads > 1
-if threads > 1:
-    resources_cmd = "-pe smp {threads}"
-else:
-    resources_cmd = ""
-# add memory limit/request to resources
-resources_cmd += " -l s_vmem={mem_per_thread}M -l m_mem_free={mem_per_thread}M"
-# if runtime specified, use it
-if runtime:
-    # make sure it is integer
-    runtime = int(runtime)
-    # runtime needs to be specified in HH:MM:SS, but is currently in minutes
-    runtime_hr = runtime // 60
-    runtime_min = runtime % 60
-    # add to resources command
-    resources_cmd += " -l h_rt={runtime_hr}:{runtime_min}:00"
-# if resources are large, perform reservation
-if (
-        threads >= int({{cookiecutter.reserve_min_threads}})
-        or
-        mem_mb >= int({{cookiecutter.reserve_min_mem_mb}})
-):
-    resources_cmd += " -R yes"
-
-# get queue part of command (if empty, don't put in anything)
-queue_cmd = "-q {queue}" if queue else ""
-
-# get cluster commands to pass through, if any
-cluster_cmd = " ".join(sys.argv[1:-1])
 
 # get command to do cluster command (no sync)
 submit_cmd = "qsub -terse -cwd"
 
+# get queue part of command (if empty, don't put in anything)
+queue_cmd = "-q {queue}" if "{{cookiecutter.default_queue}}" else ""
+
+# get resources
+res_cmd = generate_resources_command(job)
+
+# get logs
+log_cmd = get_log_files(job)
+
+# get cluster commands to pass through, if any
+cluster_cmd = " ".join(sys.argv[1:-1])
+
 # format command
-cmd = "{submit} {resources} {jobinfo} {queue} {cluster} {jobscript}".format(
+cmd = "{submit} {queue} {res} {log} {cluster} {jobscript}".format(
     submit=submit_cmd,
-    resources=resources_cmd,
-    jobinfo=jobinfo_cmd,
     queue=queue_cmd,
+    res=res_cmd,
+    log=log_cmd,
     cluster=cluster_cmd,
     jobscript=jobscript
 )
